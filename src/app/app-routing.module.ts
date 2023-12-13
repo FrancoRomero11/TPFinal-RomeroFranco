a37a5def00df95113733cb08b25433f622ebcf9a
Franco Romero <romerofran151@gmail.com>
2023-12-13 17:05:46 -0300

    lazyloading (ruta principal: app-routing) en administracion, catalogo e inicio y rutas secundarias de los mismos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,25 @@ import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component'
 
 const routes: Routes = [
   //lazyloading
-{
-  path:"",component:InicioComponent
-},
-  {path:"auth", 
-  loadChildren:()=>import('./modules/auth/auth.module').then(m =>m.AuthModule)
-}
-
+  {
+    path: "", component: InicioComponent
+  },
+  {
+    path: "auth",
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: "inicio",
+    loadChildren: () => import('./modules/inicio/inicio.module').then(m => m.InicioModule)
+  },
+  {
+    path: "catalogo",
+    loadChildren: () => import('./modules/catalogo/catalogo.module').then(m => m.CatalogoModule)
+  },
+  {
+    path: "administracion",
+    loadChildren: () => import('./modules/administracion/administracion.module').then(m => m.AdministracionModule)
+  },
 ];
 
 @NgModule({
